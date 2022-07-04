071968795d0a137d887e2044194837e5b0c6f7a1
MKish420 <meaghanmpeters@hotmail.com>
2022-07-04 13:19:40 -0400

    unable to color change

--- script.js
+++ script.js
@@ -5,69 +5,41 @@ console.log(time);
 
 //end current date and time header>
 
-    
-$(document).ready(function() {
-    $("#saveBtn").click(function( event ) {
-    var input = $("#input").val();
-    $(".9").append(input);
+
+$(document).ready(function () {
+    $("#saveBtn").click(function (event) {
+        var input = $("#input").val();
+        $(".9").append(input);
     });
-    $(".9").click(function( event ) {
+    $(".9").click(function (event) {
     });
 
 
 
 
 
-//funtion to save data to local storage
-// var saveBtn = $('#saveBtn');
-// saveBtn.on('click', function() {
-//     var input = $('#input').val();
-//     var time = $('9').val();
-// });
+    //funtion to save data to local storage
+    // var saveBtn = $('#saveBtn');
+    // saveBtn.on('click', function() {
+    //     var input = $('#input').val();
+    //     var time = $('9').val();
+    // });
 
-// grab value for each box
+    // grab value for each box
 
-//save value to local storage setelement
+    //save value to local storage setelement
 
-//getting prvious value getelement 
+    //getting prvious value getelement 
 
-//display value on page from local storage 
- 
-// function for changing background color based on time of day 
-function changeColorTime() {
-    var currentTime = new Date().getHours();
-    if (currentTime < 8) {
-        $('body').css('background-color', '#ffb3b3');
-    } else if (currentTime < 12) {
-        $('body').css('background-color', '#ffcccc');
-    } else if (currentTime < 16) {
-        $('body').css('background-color', '#ffcccc');
-    } else if (currentTime < 20) {
-        $('body').css('background-color', '#ffcccc');
-    } else {
-        $('body').css('background-color', '#ffcccc');
-    }
-}
-//     var currentTime = Date().getHours();
-//     if (currentTime < 8) {
-//         $("body").css("background-color", "black");
-//     } else if (currentTime < 16) {
-//         $("body").css("background-color", "green");
-//     } else if (currentTime < 24) {
-//         $("body").css("background-color", "blue");
-//     } else {
-//         $("body").css("background-color", "red");
-//     }
-// }
-// timeOfDay();
-// });
+    //display value on page from local storage 
 
-
-//compare current time of day with time associated with each block 
-// use moment.js for current time 
-
-//var = to current time of day using moment
-//loop over blocks compare current time 
-// if its true turn color of block a color 
-// else if the blocks time is equal set to red. 
-// else set to green/blue/black
+    // function for changing background color based on time of day 
+    function changeColorTime() {
+        var currentTime = momentjscurrentTime.format('H');
+        ;
+        for (let i = 0; i < 9; i++) {
+            if (currentTime > i) {
+                $('9').css('background-color', '#f5f5f5');
+            } else {
+                $('9').css('background-color', '#ff0000');
+            }
