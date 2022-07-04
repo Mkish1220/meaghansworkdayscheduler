d94d751d90cc312d3146e0fe39a82b296c5a8621
MKish420 <meaghanmpeters@hotmail.com>
2022-07-04 19:43:12 -0400

    added js

--- script.js
+++ script.js
@@ -2,44 +2,17 @@
 var time = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(time);
 console.log(time);
-
 //end current date and time header>
-
-
-$(document).ready(function () {
-    $("#saveBtn").click(function (event) {
-        var input = $("#input").val();
-        $(".9").append(input);
-    });
-    $(".9").click(function (event) {
-    });
-
-
-
-
-
-    //funtion to save data to local storage
-    // var saveBtn = $('#saveBtn');
-    // saveBtn.on('click', function() {
-    //     var input = $('#input').val();
-    //     var time = $('9').val();
-    // });
-
-    // grab value for each box
-
-    //save value to local storage setelement
-
-    //getting prvious value getelement 
-
-    //display value on page from local storage 
-
-    // function for changing background color based on time of day 
-    function changeColorTime() {
-        var currentTime = momentjscurrentTime.format('H');
-        ;
-        for (let i = 0; i < 9; i++) {
-            if (currentTime > i) {
-                $('9').css('background-color', '#f5f5f5');
-            } else {
-                $('9').css('background-color', '#ff0000');
-            }
+localStorage.setItem("input", "");
+localStorage.getItem("input");
+//function to save input>
+document.getElementById("saveBtn").addEventListener("click", displayText);
+function displayText() {
+    var input = document.getElementById("input").value;
+    localStorage.setItem("input", input);
+}
+//function to display input>
+function displayInput() {
+    var input = localStorage.getItem("input");
+    document.getElementById("input").value = input;
+}
